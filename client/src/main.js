e2a95393e252f83bfa625c31d169f40138fac25a
williamlion <liaohan0203@gmail.com>
2020-07-06 20:47:58 -0700

    init client

---
+++ client/src/main.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import App from './App.vue'
+import ViewUI from 'view-design'
+import 'view-design/dist/styles/iview.css'
+
+Vue.use(VueRouter)
+Vue.use(ViewUI)
+
+// The routing configuration
+const RouterConfig = {
+    routes: Routers
+}
+const router = new VueRouter(RouterConfig)
+
+Vue.config.productionTip = false
+
+new Vue({
+    router: router,
+    render: h => h(App),
+}).$mount('#app')
